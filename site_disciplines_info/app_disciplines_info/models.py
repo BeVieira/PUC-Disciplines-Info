02c83e986e4a7f20c1a3294ef2b6859246baf360
BeVieira <bernardo.vs245@gmail.com>
2024-10-15 16:50:59 -0300

    feat: Migrações + Serviço de busca + Dados do banco

--- site_disciplines_info/app_disciplines_info/models.py
+++ site_disciplines_info/app_disciplines_info/models.py
@@ -1,40 +1,35 @@
-from django.db import models
-
-# Create your models here.
+#python manage.py makemigrations
+#python manage.py migrate
 from django.db import models
 
 class Professor(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField()
-    site = models.URLField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nome
+    email = models.EmailField(blank=True, null=True)
 
 class Disciplina(models.Model):
     codigo = models.CharField(max_length=10)
     nome = models.CharField(max_length=100)
-    dia_horario = models.CharField(max_length=50)
+    dia = models.CharField(max_length=50)
+    horario = models.CharField(max_length=50)
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='disciplinas')
 
-    def __str__(self):
-        return f"{self.codigo} - {self.nome}"
-
-class Aluno(models.Model):
-    nome = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nome
-
 class Review(models.Model):
-    nota = models.IntegerField()
-    data = models.DateField()
-    metodo_avaliacao = models.CharField(max_length=100)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE, related_name='reviews')
+    autor = models.CharField(max_length=100)
+    dificuldade = models.FloatField()
+    criadoEm = models.DateField()
     estilo_aula = models.CharField(max_length=100)
     presenca = models.BooleanField()
-    dificuldade = models.CharField(max_length=100)
-    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name='reviews')
-    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE, related_name='reviews')
+    
+class AvaliacaoTipo(models.Model):
+    tipo = models.CharField(max_length=100)
 
-    def __str__(self):
-        return f"Review de {self.aluno} para {self.disciplina}"
+class QtdAvaliacao(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='avaliacoes')
+    tipo = models.ForeignKey(AvaliacaoTipo, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+
+''' Deixar para depois (Caso necessário)
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+'''
