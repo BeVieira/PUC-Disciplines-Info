ccdfaf2e189722f445361ac3d5888d8f9b2a88fd
BeVieira <bernardo.vs245@gmail.com>
2024-11-04 11:13:21 -0300

    Busca criada

--- site_disciplines_info/app_disciplines_info/views/views.py
+++ site_disciplines_info/app_disciplines_info/views/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from app_disciplines_info.models import Disciplina, Professor
 
-# Create your views here.
+def consultar_view(request):
+    if request.method == 'POST':
+        consulta = None
+        resultado = {}
+        codigo = request.POST.get('codigo')
+        nome = request.POST.get('nome')
+        professor = request.POST.get('professor')
+
+        if codigo:
+            consulta = Disciplina.objects.filter(codigo=codigo)
+        else:
+            if nome:
+                consulta = Disciplina.objects.filter(nome__icontains=nome)  
+            else:
+                consulta = Disciplina.objects.filter(professor__nome__icontains=professor)
+
+        for disciplina in consulta:
+            chave = (disciplina.codigo, disciplina.nome, disciplina.professor.nome)
+            if chave not in resultado:
+                resultado[chave] = []
+            resultado[chave].append(f'{disciplina.dia} / {disciplina.horario}')
+
+        return render(request, 'consulta.html', {'resultado': resultado})
+    
+    return render(request, 'consulta.html')
+
